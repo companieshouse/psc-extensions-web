361b7d8cfa6fbed611e033327f5fc50ab015fb17
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-07-02 11:17:28 +0100

    null code

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -6,7 +6,4 @@ const logger: ApplicationLogger = createLogger(process.env.APP_NAME ?? "");
 // tslint:disable-next-line:no-console
 console.log(`env.LOG_LEVEL set to ${process.env.LOG_LEVEL}`);
 
-  debugRequest (req: Request, message: string) {
-        this.raw.debugRequest(req, `${this.getPrefix()} ${message}`);
-    }
 export default logger;
