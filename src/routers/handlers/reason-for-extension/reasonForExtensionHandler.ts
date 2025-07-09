692bfa9648acc123258a3895e57fc77ccbe4a174
marychacia-ch <mchacia@companieshouse.gov.uk>
2025-07-09 16:06:46 +0100

    Reason for extension @mchacia

---
+++ src/routers/handlers/reason-for-extension/reasonForExtensionHandler.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { BaseViewData, GenericHandler, ViewModel } from "../generic";
+import logger from "../../../lib/Logger";
+import { Urls } from "../../../lib/constants";
+
+export class ReasonForExtensionHandler extends GenericHandler<BaseViewData> {
+
+    public static templatePath = "router_views/reason-for-extension/reason-for-extension";
+
+    public async getViewData (req: Request, res: Response): Promise<BaseViewData> {
+        const baseViewData = await super.getViewData(req, res);
+
+        return {
+            ...baseViewData,
+            // TODO: Add search params to backURL
+            backURL: Urls.EXTENSION_INFO_URL,
+            templateName: Urls.REASON_FOR_EXTENSION_PAGE
+        };
+    }
+
+    public async executeGet (req: Request, res: Response): Promise<ViewModel<BaseViewData>> {
+        logger.info(`called to serve start page`);
+
+        // ...process request here and return data for the view
+        return {
+            templatePath: ReasonForExtensionHandler.templatePath,
+            viewData: await this.getViewData(req, res)
+        };
+    }
+}
