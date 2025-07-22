d749c127edd5d5f2e7c2bba3b90e181979dce40c
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-07-22 11:27:05 +0100

    renaming file to match

--- src/httpServer.ts
+++ src/httpServer.ts
@@ -1,15 +1,15 @@
 import fs from "fs";
 import http from "http";
 import https from "https";
-import logger from "./lib/Logger";
 import app from "./app";
+import { logger } from "./lib/logger";
 
 // start the HTTP server
 const httpServer = http.createServer(app);
 httpServer.listen(process.env.NODE_PORT, () => {
     console.log(`Server started at: ${process.env.NODE_HOSTNAME}:${process.env.NODE_PORT}`);
 }).on("error", err => {
-    logger.error(`${err.name} - HTTP Server error: ${err.message} - ${err.stack}`);
+    logger.error(`HTTP Server error: ${err.message} - ${err.stack}`);
 });
 
 // Start the secure server - possibly not required if app is running behind a loadbalancer, with SSL termination
@@ -23,7 +23,7 @@ if (process.env.NODE_SSL_ENABLED === "ON") {
     httpsServer.listen(process.env.NODE_PORT_SSL, () => {
         console.log(`Secure server started at: ${process.env.NODE_HOSTNAME_SECURE}:${process.env.NODE_PORT_SSL}`);
     }).on("error", err => {
-        logger.error(`${err.name} - HTTPS Server error: ${err.message} - ${err.stack}`);
+        logger.error(`HTTPS Server error: ${err.message} - ${err.stack}`);
     });
 }
 
