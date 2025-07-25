42745a452c03c6a3883362e329cb73fd638e0146
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-07-25 11:01:03 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -36,7 +36,7 @@ app.set("view engine", "njk");
 
 // Serve static files
 app.use(express.static(path.join(__dirname, "/../assets/public")));
-app.use("/assets", express.static(path.join(__dirname, "../node_modules/govuk-frontend/govuk/assets")));
+app.use("/assets", express.static(path.join(__dirname, "../node_modules/govuk-frontend/dist/govuk/assets")));
 
 njk.addGlobal("cdnUrlCss", process.env.CDN_URL_CSS);
 njk.addGlobal("cdnUrlJs", process.env.CDN_URL_JS);
