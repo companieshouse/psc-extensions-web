67db981505fbf192115884fce6429701c05956db
marychacia-ch <mchacia@companieshouse.gov.uk>
2025-08-05 15:10:14 +0100

    further fixes

--- assets/src/js/backButton.js
+++ assets/src/js/backButton.js
@@ -1,4 +1,7 @@
-alert('hello');
-window.addEventListener("popstate", (event) => {
+history.replaceState('', '', '/psc-extensions/extension-already-submitted');
+history.pushState('', '', window.location.href);
+window.addEventListener('popstate', () => {
+    alert('hello');
     window.location.href = '/psc-extensions/extension-already-submitted'
 });
+
