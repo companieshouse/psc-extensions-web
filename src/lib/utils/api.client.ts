5ed988b9d313b06c0fc9fcb523dcd398c4ce4c35
tferns-ch <tferns@companieshouse.gov.uk>
2025-08-22 15:26:19 +0100

    fix(deep-367-362): temp api client

--- src/lib/utils/api.client.ts
+++ src/lib/utils/api.client.ts
@@ -2,10 +2,10 @@ import {createApiClient} from "@companieshouse/api-sdk-node";
 import ApiClient from "@companieshouse/api-sdk-node/dist/client";
 import {Session} from "@companieshouse/node-session-handler";
 
-export const createOAuthApiClient = (session: Session): ApiClient => {
-    const accessToken = session?.data?.oauth2_nonce;
-    if (!accessToken) {
-        throw new Error("No access token found in session");
-    }
-    return createApiClient(undefined, accessToken);
+import {env} from "../../config";
+import {getAccessToken} from "./session.util";
+
+export const createOAuthApiClient = (session: Session | undefined, baseUrl: string = env.API_URL): ApiClient => {
+    if (!session) throw new Error();
+    return createApiClient(undefined, getAccessToken(session as Session), baseUrl);
 };
--- src/lib/utils/session.util.ts
+++ src/lib/utils/session.util.ts
@@ -1,8 +1,8 @@
-import { Session } from "@companieshouse/node-session-handler";
-import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
-import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
-import { UserProfileKeys } from "@companieshouse/node-session-handler//lib/session/keys/UserProfileKeys";
-import { ISignInInfo } from "@companieshouse/node-session-handler/lib/session/model/SessionInterfaces";
+import {Session} from "@companieshouse/node-session-handler";
+import {SessionKey} from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
+import {SignInInfoKeys} from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
+import {UserProfileKeys} from "@companieshouse/node-session-handler//lib/session/keys/UserProfileKeys";
+import {ISignInInfo} from "@companieshouse/node-session-handler/lib/session/model/SessionInterfaces";
 
 const getSignInInfo = (session: Session | undefined): ISignInInfo | undefined => {
     return session?.data?.[SessionKey.SignInInfo];
@@ -12,3 +12,8 @@ export const getUserEmail = (session: Session | undefined): string => {
     const signInInfo = getSignInInfo(session);
     return signInInfo?.[SignInInfoKeys.UserProfile]?.[UserProfileKeys.Email] as string;
 };
+
+export function getAccessToken(session: Session): string {
+    const signInInfo = getSignInInfo(session);
+    return signInInfo?.[SignInInfoKeys.AccessToken]?.[AccessTokenKeys.AccessToken] as string;
+}
