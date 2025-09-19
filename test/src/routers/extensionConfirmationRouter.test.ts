e5da2927aba8f9c5d242864c7e35a61959a5f0e6
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-09-19 14:11:34 +0100

    Add test

--- test/src/routers/extensionConfirmationRouter.test.ts
+++ test/src/routers/extensionConfirmationRouter.test.ts
@@ -17,6 +17,12 @@ describe("GET extension confirmation router", () => {
         expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
     });
 
+    it("should check session and user auth before returning the page", async () => {
+        await router.get(SERVICE_PATH_PREFIX + PATHS.EXTENSION_CONFIRMATION_SECOND);
+        expect(mocks.mockSessionMiddleware).toHaveBeenCalled();
+        expect(mocks.mockAuthenticationMiddleware).toHaveBeenCalled();
+    });
+
     it("should return status 200", async () => {
         await router.get(SERVICE_PATH_PREFIX + PATHS.EXTENSION_CONFIRMATION_FIRST).expect(200);
     });
