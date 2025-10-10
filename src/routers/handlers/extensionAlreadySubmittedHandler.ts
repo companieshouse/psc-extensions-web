ea614c84497531b3b8aea08d7ec726f4fe0010c2
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-10-10 16:00:58 +0100

    company links

--- src/routers/handlers/extensionAlreadySubmittedHandler.ts
+++ src/routers/handlers/extensionAlreadySubmittedHandler.ts
@@ -2,19 +2,29 @@ import { Request, Response } from "express";
 import { BaseViewData, GenericHandler, ViewModel } from "./abstractGenericHandler";
 import logger from "../../lib/logger";
 import { SERVICE_PATH_PREFIX, PATHS, ROUTER_VIEWS_FOLDER_PATH } from "../../lib/constants";
+import { getCompanyProfile } from "../../services/companyProfileService";
 
-export class ExtensionAlreadySubmittedHandler extends GenericHandler<BaseViewData> {
+interface PscViewData extends BaseViewData {
+    companyName: string;
+    companyNumber: string;
+}
+export class ExtensionAlreadySubmittedHandler extends GenericHandler<PscViewData> {
 
-    protected override async getViewData (req: Request, res: Response): Promise<BaseViewData> {
+    protected override async getViewData (req: Request, res: Response): Promise<PscViewData> {
         const baseViewData = await super.getViewData(req, res);
+        const companyNumber = req.query.companyNumber as string;
+        const companyProfile = await getCompanyProfile(req, companyNumber);
+
         return {
             ...baseViewData,
             backURL: SERVICE_PATH_PREFIX + PATHS.FIRST_EXTENSION_CONFIRMATION,
-            templateName: PATHS.EXTENSION_ALREADY_SUBMITTED.slice(1)
+            templateName: PATHS.EXTENSION_ALREADY_SUBMITTED.slice(1),
+            companyName: companyProfile.companyName,
+            companyNumber: companyProfile.companyNumber
         };
     }
 
-    public async executeGet (req: Request, res: Response): Promise<ViewModel<BaseViewData>> {
+    public async executeGet (req: Request, res: Response): Promise<ViewModel<PscViewData>> {
         logger.info(`called to serve start page`);
         return {
             templatePath: ROUTER_VIEWS_FOLDER_PATH + PATHS.EXTENSION_ALREADY_SUBMITTED,
