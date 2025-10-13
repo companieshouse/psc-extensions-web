4cfaa93f4aefa5a5568594ccfd1745b37b75ecc3
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-10-13 16:50:41 +0100

    last handler

--- src/routers/handlers/extensionAlreadySubmittedHandler.ts
+++ src/routers/handlers/extensionAlreadySubmittedHandler.ts
@@ -7,18 +7,21 @@ import { getCompanyProfile } from "../../services/companyProfileService";
 interface PscViewData extends BaseViewData {
     companyName: string;
     companyNumber: string;
+    selectedPscId: string;
 }
 export class ExtensionAlreadySubmittedHandler extends GenericHandler<PscViewData> {
 
     protected override async getViewData (req: Request, res: Response): Promise<PscViewData> {
         const baseViewData = await super.getViewData(req, res);
         const companyNumber = req.query.companyNumber as string;
+        const selectedPscId = req.query.selectedPscId as string;
         const companyProfile = await getCompanyProfile(req, companyNumber);
 
         return {
             ...baseViewData,
-            backURL: SERVICE_PATH_PREFIX + PATHS.FIRST_EXTENSION_CONFIRMATION,
+            backURL: SERVICE_PATH_PREFIX + PATHS.FIRST_EXTENSION_CONFIRMATION + "?companyNumber=" + companyNumber + "&selectedPscId=" + selectedPscId + "%3F",
             templateName: PATHS.EXTENSION_ALREADY_SUBMITTED.slice(1),
+            selectedPscId: selectedPscId,
             companyName: companyProfile.companyName,
             companyNumber: companyProfile.companyNumber
         };
