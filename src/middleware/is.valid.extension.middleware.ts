ad11780c8478909930e2870c236dc1ac3de43e86
jmccoubrey-ch <jmccoubrey@companieshouse.gov.uk>
2025-11-13 12:53:40 +0000

    Have log ready for api-sdk-node upgrade

--- src/middleware/is.valid.extension.middleware.ts
+++ src/middleware/is.valid.extension.middleware.ts
@@ -35,7 +35,8 @@ export const validateExtensionRequest = handleExceptions(async (req: Request, re
         const isValid = validationResponse.valid;
 
         if (!isValid) {
-            logger.error(`Validation failed for PSC ID: ${pscNotificationId}. Validation Errors: ${JSON.stringify(validationResponse.errors)}`);
+            // TODO: uncomment this once we have updated to the most recent version of api-sdk-node
+            // logger.error(`Validation failed for PSC ID: ${pscNotificationId}. Validation Errors: ${JSON.stringify(validationResponse.validationStatusError)}`);
             return res.redirect(addSearchParams(PREFIXED_URLS.EXTENSION_REFUSED, { companyNumber, selectedPscId: pscNotificationId }));
         }
 
