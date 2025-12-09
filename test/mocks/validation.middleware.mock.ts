1cbae55c7dcb793e17543b3f45d1681a31cd8f12
cbroadley-ch <cbroadley@companieshouse.gov.uk>
2025-12-09 10:43:27 +0000

    Content update and lint fixes

--- test/mocks/validation.middleware.mock.ts
+++ test/mocks/validation.middleware.mock.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const mockValidationMiddleware = jest.fn((req: Request, res: Response, next: NextFunction) => {
     return next();
