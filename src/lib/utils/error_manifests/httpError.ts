97d88dc45ceeac2650d56427c8a647098734c016
AlisonGriffiths <agriffiths1@companieshouse.gov.uk>
2026-04-02 13:56:06 +0100

    Updated module and rootDir config in tsconfig.json and fixed issues

--- src/lib/utils/error_manifests/httpError.ts
+++ src/lib/utils/error_manifests/httpError.ts
@@ -3,10 +3,10 @@ import { HttpStatusCode } from "axios";
 export class HttpError extends Error {
     public status: number;
 
-    constructor (message: string, status: number | HttpStatusCode) {
+    constructor (message: string, status: number | (typeof HttpStatusCode)[keyof typeof HttpStatusCode]) {
         super(message);
         this.name = "HttpError";
-        this.status = status;
+        this.status = status as number;
 
         // Maintains stack trace for where the error was thrown
         if (Error.captureStackTrace) {
