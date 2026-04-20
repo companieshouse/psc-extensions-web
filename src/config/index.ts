e483c8b1ba8117ce7c5d97f2efc3424fc895bef1
AlisonGriffiths <agriffiths1@companieshouse.gov.uk>
2026-04-20 16:44:48 +0100

    ASM-1990 Adding open-telemetry configuration i#	modified:   src/config/index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,12 @@
 export const env = {
     API_URL: process.env.API_URL as string,
     APP_NAME: process.env.APP_NAME as string,
+    CHS_URL: process.env.CHS_ENV as string,
+    CHS_INTERNAL_API_KEY: process.env.CHS_INTERNAL_API_KEY as string,
     LOG_LEVEL: process.env.LOG_LEVEL as string,
     LOCALES_ENABLED: process.env.LOCALES_ENABLED as string,
     LOCALES_PATH: process.env.LOCALES_PATH as string,
-    CHS_INTERNAL_API_KEY: process.env.CHS_INTERNAL_API_KEY as string
+    NODE_ENV: process.env.NODE_ENV as string,
+    OTEL_EXPORTER_OTLP_ENDPOINT: process.env.OTEL_EXPORTER_OTLP_ENDPOINT as string,
+    OTEL_LOG_ENABLED: process.env.OTEL_LOG_ENABLED as string
 };
